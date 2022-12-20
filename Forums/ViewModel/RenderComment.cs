02ed5e46aaca757bf5c8fc34a40140d3ca220c9b
Admin <Admin@DESKTOP-7314GBN>
2022-12-20 12:41:34 +0200

    add partial view to render comment

---
+++ Forums/ViewModel/RenderComment.cs
@@ -0,0 +1,9 @@
+﻿namespace Forums.ViewModel
+{
+    public class RenderComment
+    {
+        public ICollection<GetUserForums> UserForums { get; set; }
+        public bool showAllLink { get; set; }
+        public bool showReply { get; set; }
+    }
+}
